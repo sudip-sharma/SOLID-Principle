5b2d1e240b04fd947ce51d5b6098474485e6758d
sudip-sharma <sudip.20@hotmail.com>
2017-05-30 15:27:57 +0530

    Solid Principles

--- SOLID_Principles/Liskov_Substitution_Principle.cs
+++ SOLID_Principles/Liskov_Substitution_Principle.cs
@@ -1,12 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SOLID_Principles
 {
-    class Liskov_Substitution_Principle
+    public class Liskov_Substitution_Principle
     {
+        #region Implementation without LSP 
+
+        //public class SqlFile
+        //{
+        //    public string FilePath { get; set; }
+        //    public string FileText { get; set; }
+        //    public string LoadText()
+        //    {
+        //        return string.Empty;
+        //    }
+        //    public string SaveText()
+        //    {
+        //        return string.Empty;
+        //    }
+        //}
+        //public class SqlFileManager
+        //{
+        //    public List<SqlFile> lstSqlFiles { get; set; }
+
+        //    public string GetTextFromFiles()
+        //    {
+        //        StringBuilder objStrBuilder = new StringBuilder();
+        //        foreach (var objFile in lstSqlFiles)
+        //        {
+        //            objStrBuilder.Append(objFile.LoadText());
+        //        }
+        //        return objStrBuilder.ToString();
+        //    }
+        //    public void SaveTextIntoFiles()
+        //    {
+        //        foreach (var objFile in lstSqlFiles)
+        //        {
+        //            objFile.SaveText();
+        //        }
+        //    }
+        //}
+
+        //-----Now if we have to add additional functionality of checking for readonly files-----
+
+        //public class SqlFile
+        //{
+        //    public string LoadText()
+        //    {
+        //        return string.Empty;
+        //    }
+        //    public void SaveText()
+        //    {
+        //        /* Code to save text into sql file */
+        //    }
+        //}
+        //public class ReadOnlySqlFile : SqlFile
+        //{
+        //    public string FilePath { get; set; }
+        //    public string FileText { get; set; }
+        //    public string LoadText()
+        //    {
+        //        return string.Empty;
+        //    }
+        //    public void SaveText()
+        //    {
+        //        /* Throw an exception when app flow tries to do save. */
+        //        throw new IOException("Can't Save");
+        //    }
+        //}
+
+        //public class SqlFileManager
+        //{
+        //    public List<SqlFile> lstSqlFiles { get; set; }
+        //    public string GetTextFromFiles()
+        //    {
+        //        StringBuilder objStrBuilder = new StringBuilder();
+        //        foreach (var objFile in lstSqlFiles)
+        //        {
+        //            objStrBuilder.Append(objFile.LoadText());
+        //        }
+        //        return objStrBuilder.ToString();
+        //    }
+        //    public void SaveTextIntoFiles()
+        //    {
+        //        foreach (var objFile in lstSqlFiles)
+        //        {
+        //            //Check whether the current file object is read only or not.If yes, skip calling it's  
+        //            // SaveText() method to skip the exception.  
+
+        //            if (objFile is ReadOnlySqlFile)
+        //                objFile.SaveText();
+        //        }
+        //    }
+        //}
+
+        #endregion
+
+        #region Implementation with LSP
+
+        public interface IReadableSqlFile
+        {
+            string LoadText();
+        }
+        public interface IWritableSqlFile
+        {
+            void SaveText();
+        }
+
+        public class ReadOnlySqlFile : IReadableSqlFile
+        {
+            public string FilePath { get; set; }
+            public string FileText { get; set; }
+            public string LoadText()
+            {
+                return string.Empty;
+            }
+        }  
+
+        public class SqlFile : IWritableSqlFile, IReadableSqlFile
+        {
+            public string FilePath { get; set; }
+            public string FileText { get; set; }
+            public string LoadText()
+            {
+                return string.Empty;
+            }
+            public void SaveText()
+            {
+                /* Code to save text into sql file */
+            }
+
+            public class SqlFileManager
+            {
+                public string GetTextFromFiles(List<IReadableSqlFile> aLstReadableFiles)
+                {
+                    StringBuilder objStrBuilder = new StringBuilder();
+                    foreach (var objFile in aLstReadableFiles)
+                    {
+                        objStrBuilder.Append(objFile.LoadText());
+                    }
+                    return objStrBuilder.ToString();
+                }
+                public void SaveTextIntoFiles(List<IWritableSqlFile> aLstWritableFiles)
+                {
+                    foreach (var objFile in aLstWritableFiles)
+                    {
+                        objFile.SaveText();
+                    }
+                }
+            }   
+        } 
+
+        #endregion
     }
 }
--- SOLID_Principles/Open_Closed_Principle.cs
+++ SOLID_Principles/Open_Closed_Principle.cs
@@ -6,7 +6,104 @@ using System.Threading.Tasks;
 
 namespace SOLID_Principles
 {
-    class Open_Closed_Principle
+    public class Open_Closed_Principle
     {
+        #region Implementation Without O/C principle
+        //public class Rectangle
+        //{
+        //    public int Height { get; set; }
+        //    public int Width { get; set; }
+        //}
+
+        //public class Circle
+        //{
+        //    public double Radius { get; set; }
+        //}
+
+        //public class CalculateArea
+        //{
+        //    public double TotalArea(Rectangle[] arrRectangles)
+        //    {
+        //        double area = 0;
+        //        foreach (var rectangleArea in arrRectangles)
+        //        {
+        //            area += rectangleArea.Height * rectangleArea.Width;
+        //        }
+
+        //        return area;
+        //    }
+        //}       
+
+        #region If we want to add the area calculation of a circle as well to the exsisting above code
+
+        //public class CalculateArea
+        //{
+        //    public double TotalArea(object[] arrObject)
+        //    {
+        //        double area = 0;
+        //        Rectangle objRectangle;
+        //        Circle objCircle;
+        //        foreach (var obj in arrObject)
+        //        {
+        //            if (obj is Rectangle)
+        //            {
+        //                objRectangle = (Rectangle)obj;
+        //                area += objRectangle.Height * objRectangle.Width;
+        //            }
+        //            else
+        //            {
+        //                objCircle = (Circle)obj;
+        //                area += objCircle.Radius * objCircle.Radius * Math.PI;
+        //            }
+        //        }
+
+        //        return area;
+        //    }
+        //}
+
+        #endregion
+
+        #endregion
+
+        #region Still the above code is not following the O/C principle as each time there is a change the CalculateArea method will be changed
+
+        public abstract class Shape
+        {
+            public abstract double Area();
+        }
+
+        public class Rectangle : Shape
+        {
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public override double Area()
+            {
+                return Height * Width;
+            }
+        }
+
+        public class Circle : Shape
+        {
+            public double Radius { get; set; }
+            public override double Area()
+            {
+                return Radius * Radius * Math.PI;
+            }
+        }
+
+        public class CalculateArea
+        {
+            public double TotalArea(Shape[] objShape)
+            {
+                double area = 0;
+                foreach (var obj in objShape)
+                {
+                    area += obj.Area();
+                }
+                return area;
+            }
+        }
+
+        #endregion
     }
 }
